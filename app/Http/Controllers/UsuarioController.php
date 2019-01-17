6db6dd5051290888e51940d294944a9265e1455f
dagoberto <dpailahueque01@gmail.com>
2019-01-17 15:58:36 -0300

    quite el log

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -48,7 +48,7 @@ class UsuarioController extends Controller
 
 		$us = User::where('run', $rq->run)
 				->first();
-		dd(count($us));
+		dd(isset($us));
 
 		if(count($us)==0)
 			return redirect('/')->with('error', 'Error LOGIN!'); 
